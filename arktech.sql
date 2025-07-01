0126d143b1b64af965a3d34e7c0834f50198f591
FineCactus <157436703+FineCactus@users.noreply.github.com>
2025-07-01 21:15:02 +0530

    database backup

---
+++ arktech.sql
@@ -0,0 +1,72 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Jul 01, 2025 at 03:44 PM
+-- Server version: 9.1.0
+-- PHP Version: 8.3.14
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `arktech`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_adminlogin`
+--
+
+DROP TABLE IF EXISTS `tbl_adminlogin`;
+CREATE TABLE IF NOT EXISTS `tbl_adminlogin` (
+  `login_id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  PRIMARY KEY (`login_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `tbl_adminlogin`
+--
+
+INSERT INTO `tbl_adminlogin` (`login_id`, `username`, `password`) VALUES
+(3, 'an', '1'),
+(4, 'michael', '123321');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_district`
+--
+
+DROP TABLE IF EXISTS `tbl_district`;
+CREATE TABLE IF NOT EXISTS `tbl_district` (
+  `district_id` int NOT NULL AUTO_INCREMENT,
+  `district_name` varchar(30) NOT NULL,
+  UNIQUE KEY `district_id` (`district_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `tbl_district`
+--
+
+INSERT INTO `tbl_district` (`district_id`, `district_name`) VALUES
+(1, 'Idukki'),
+(2, 'Ernakulam'),
+(3, 'Thrissur'),
+(4, 'Kasargod');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
